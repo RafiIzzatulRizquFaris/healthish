a8a4b0ea53c2f60030b5240d568432ae0eb2bd74
quifar <risqufaris@gmail.com>
2020-10-29 06:46:53 +0700

    create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class Constants{
+  static const Color whiteColor = Color(0xffFFFFFF);
+  static const Color greyColor = Color(0xffD0D0D0);
+  static const Color blueColor = Color(0xff2962FF);
+  static const Color blackColor = Color(0xff1E1E1D);
+}
