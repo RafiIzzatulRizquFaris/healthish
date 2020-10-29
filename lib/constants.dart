04cb9496108470a2240d7ea6e49428417a8700d2
quifar <risqufaris@gmail.com>
2020-10-29 19:55:32 +0700

    add some constants variable

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,4 +5,6 @@ class Constants{
   static const Color greyColor = Color(0xffD0D0D0);
   static const Color blueColor = Color(0xff2962FF);
   static const Color blackColor = Color(0xff1E1E1D);
+  static const String eventCollections = "event";
+  static const String aboutCollections = "about";
 }
