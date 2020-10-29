049ccebd8775c8fbbdcafd9792bb890fe05dbb17
quifar <risqufaris@gmail.com>
2020-10-29 19:42:42 +0700

    create event_contract.dart

---
+++ lib/contract/event_contract.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class EventContractView{
+  onSuccessEventData(List<DocumentSnapshot> value){}
+  onErrorEventData(error){}
+}
+
+class EventContractPresenter{
+  getEventData(){}
+  loadEventData(){}
+}
