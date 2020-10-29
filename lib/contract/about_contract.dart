d2459efc12dc8d018c4fe91c9ba00288360b95d9
quifar <risqufaris@gmail.com>
2020-10-29 22:29:21 +0700

    create about_contract.dart

---
+++ lib/contract/about_contract.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AboutContractView{
+  onSuccessAboutData(List<DocumentSnapshot> value){}
+  onErrorAboutData(error){}
+}
+
+class AboutContractPresenter{
+  getAboutData(){}
+  loadAboutData(){}
+}
