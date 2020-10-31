066338c8a8b0b817df944d33c8e666db6b1dcf91
quifar <risqufaris@gmail.com>
2020-10-31 18:49:20 +0700

    add layout (not fix yet) on layanan.dart

--- lib/navigation_screen/layanan.dart
+++ lib/navigation_screen/layanan.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:healthish/constants.dart';
 
 class Layanan extends StatefulWidget {
   @override
@@ -9,14 +11,254 @@ class Layanan extends StatefulWidget {
 }
 
 class LayananState extends State<Layanan> {
+  TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Layanan",
+      backgroundColor: Constants.whiteColor,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(90),
+        child: Container(
+          alignment: Alignment.bottomLeft,
+          padding: EdgeInsets.all(20),
+          child: Text(
+            "Layanan",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Constants.blackColor,
+              fontWeight: FontWeight.w800,
+              fontSize: 32,
+            ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                controller: searchController,
+                style: TextStyle(
+                  color: Constants.blackColor,
+                ),
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  fillColor: Constants.whiteColor,
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Constants.greyColor,
+                  ),
+                  hintText: 'Search dokter, fasilitas, layanan',
+                  hintStyle: TextStyle(
+                    color: Constants.greyColor,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      color: Constants.greyColor,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      color: Constants.greyColor,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2.2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 20,
+                      right: 20,
+                      left: 20,
+                    ),
+                    child: Text(
+                      "Fasilitas & Layanan Terkini",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w800,
+                        color: Constants.blackColor,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          top: 30, left: 10, right: 20, bottom: 40),
+                      child: Container(
+                        child: ListView.builder(
+                          itemCount: 3,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: itemBuilderFacility,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 20,
+                    right: 20,
+                    left: 20,
+                  ),
+                  child: Text(
+                    "Event & Promo",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w800,
+                      color: Constants.blackColor,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 30,
+                    left: 20,
+                    right: 20,
+                    bottom: 40,
+                  ),
+                  child: Container(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: eventPromoWidget(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  Widget itemBuilderFacility(BuildContext context, int index) {
+    return Container(
+      margin: EdgeInsets.only(
+        left: 10,
+        right: 10,
+      ),
+      width: MediaQuery.of(context).size.width / 2.2,
+      decoration: BoxDecoration(
+        color: Constants.whiteColor,
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+        border: Border.all(
+          color: Constants.greyColor,
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Constants.greyColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Doctor Name",
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 14,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 8,
+              right: 8,
+              bottom: 8,
+            ),
+            child: Text(
+              "Doctor Specialist",
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  List<Widget> eventPromoWidget() {
+    List<Widget> list = List<Widget>();
+    for (int i = 0; i < 3; i++) {
+      list.add(Container(
+        margin: EdgeInsets.only(
+          bottom: 15,
+        ),
+        height: MediaQuery.of(context).size.width / 2.2,
+        decoration: BoxDecoration(
+          color: Constants.whiteColor,
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+          border: Border.all(
+            color: Constants.greyColor,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Constants.greyColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Doctor Name",
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 8,
+                right: 8,
+                bottom: 8,
+              ),
+              child: Text(
+                "Doctor Specialist",
+              ),
+            ),
+          ],
+        ),
+      ));
+    }
+    return list;
   }
 }
