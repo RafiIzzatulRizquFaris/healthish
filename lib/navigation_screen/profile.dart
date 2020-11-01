e410ad7567ad9ec7c378d9f548e4c1b87e035096
quifar <risqufaris@gmail.com>
2020-11-01 19:46:09 +0700

    add tab bar in profile.dart

--- lib/navigation_screen/profile.dart
+++ lib/navigation_screen/profile.dart
@@ -1,7 +1,9 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:healthish/constants.dart';
+import 'package:healthish/custom_tab_indicator.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -10,7 +12,8 @@ class Profile extends StatefulWidget {
   }
 }
 
-class ProfileState extends State<Profile> {
+class ProfileState extends State<Profile> with SingleTickerProviderStateMixin {
+  TabController tabController;
   PreferredSize appBar = PreferredSize(
     preferredSize: Size.fromHeight(100),
     child: Container(
@@ -30,6 +33,16 @@ class ProfileState extends State<Profile> {
   );
 
   @override
+  void initState() {
+    super.initState();
+    tabController = TabController(
+      length: 2,
+      initialIndex: 0,
+      vsync: this,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Constants.whiteColor,
@@ -47,37 +60,152 @@ class ProfileState extends State<Profile> {
             ),
           ),
           SingleChildScrollView(
-            child:
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height,
-                      margin: EdgeInsets.only(
-                        top: appBar.preferredSize.height + 100,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Constants.whiteColor,
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(30),
-                          topLeft: Radius.circular(30),
-                        ),
-                      ),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  margin: EdgeInsets.only(
+                    top: appBar.preferredSize.height + 100,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Constants.whiteColor,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(30),
+                      topLeft: Radius.circular(30),
                     ),
-                    Positioned(
-                      top: appBar.preferredSize.height + 50,
-                      child: Container(
-                        height: 100,
-                        width: 100,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                            color: Constants.greyColor
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.more_horiz_rounded),
+                              color: Constants.blackColor,
+                              onPressed: () {},
+                            )
+                          ],
                         ),
-                      ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            "Name Goes Here",
+                            style: TextStyle(
+                                color: Constants.blackColor,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            bottom: 5,
+                          ),
+                          child: Text(
+                            "Laki - Laki",
+                            style: TextStyle(
+                              color: Constants.blackColor,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          "082299189919",
+                          style: TextStyle(
+                            color: Constants.greyColor,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            right: 10,
+                            left: 10,
+                            bottom: 10,
+                            top: 20,
+                          ),
+                          child: Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                              color: Constants.greyColorTab,
+                            ),
+                            child: TabBar(
+                              controller: tabController,
+                              indicatorSize: TabBarIndicatorSize.tab,
+                              indicator: CustomTabIndicator(),
+                              labelColor: Constants.blackColor,
+                              tabs: [
+                                Tab(
+                                  child: Badge(
+                                    badgeContent: Text(
+                                      '2',
+                                      style: TextStyle(
+                                        color: Constants.whiteColor,
+                                      ),
+                                    ),
+                                    badgeColor: Constants.redColor,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text("Notifikasi"),
+                                    ),
+                                  ),
+                                ),
+                                Tab(
+                                  child: Badge(
+                                    badgeContent: Text(
+                                      '1',
+                                      style: TextStyle(
+                                        color: Constants.whiteColor,
+                                      ),
+                                    ),
+                                    badgeColor: Constants.redColor,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text("Histori Booking"),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: TabBarView(
+                            controller: tabController,
+                            children: [
+                              Column(
+                                children: [
+                                  Text("data"),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Text("data"),
+                                ],
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
                     ),
-                  ],
+                  ),
                 ),
+                Positioned(
+                  top: appBar.preferredSize.height + 50,
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: Constants.greyColor),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
