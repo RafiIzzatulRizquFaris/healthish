59cb205ce903d540c304f09157648ee06ea8eea4
quifar <risqufaris@gmail.com>
2020-11-01 19:45:25 +0700

    add redColor in constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,8 +3,10 @@ import 'package:flutter/cupertino.dart';
 class Constants{
   static const Color whiteColor = Color(0xffFFFFFF);
   static const Color greyColor = Color(0xffD0D0D0);
+  static const Color greyColorTab = Color(0xffF2F2F2);
   static const Color blueColor = Color(0xff2962FF);
   static const Color blackColor = Color(0xff1E1E1D);
+  static const Color redColor = Color(0xffFF5E20);
   static const String eventCollections = "event";
   static const String aboutCollections = "about";
 }
