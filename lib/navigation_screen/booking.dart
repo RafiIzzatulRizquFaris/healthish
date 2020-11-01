9b2a3a2f9defef438a9f7ae4e7fb3aebf4a81862
quifar <risqufaris@gmail.com>
2020-11-01 10:46:38 +0700

    completing layout on booking.dart

--- lib/navigation_screen/booking.dart
+++ lib/navigation_screen/booking.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:healthish/constants.dart';
 
 class Booking extends StatefulWidget {
   @override
@@ -12,9 +13,52 @@ class BookingState extends State<Booking> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Booking",
+      backgroundColor: Constants.whiteColor,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(90),
+        child: Container(
+          alignment: Alignment.bottomLeft,
+          padding: EdgeInsets.all(20),
+          child: Text(
+            "Booking",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Constants.blackColor,
+              fontWeight: FontWeight.w800,
+              fontSize: 32,
+            ),
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: 20,
+        itemBuilder: itemBuilderDoctor,
+      ),
+    );
+  }
+
+  Widget itemBuilderDoctor(BuildContext context, int index) {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: ListTile(
+        leading: Container(
+          width: 50,
+          height: 50,
+          decoration: BoxDecoration(
+            color: Constants.greyColor,
+            borderRadius: BorderRadius.all(Radius.circular(1000)),
+          ),
+        ),
+        title: Text(
+          "Dokter $index",
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 14,
+              color: Constants.blackColor),
+        ),
+        subtitle: Text(
+          "Deskripsi singkat dokter $index",
+          style: TextStyle(color: Constants.greyColor),
         ),
       ),
     );
