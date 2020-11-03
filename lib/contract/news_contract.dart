1f0fc6da27af9c46d0987cee37f1de793e1ebe75
quifar <risqufaris@gmail.com>
2020-11-03 10:01:00 +0700

    create news_contract.dart

---
+++ lib/contract/news_contract.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class NewsContractView{
+  onSuccessNewsData(List<DocumentSnapshot> value){}
+  onErrorNewsData(error){}
+}
+
+class NewsContractPresenter{
+  getNewsData(){}
+  loadNewsData(){}
+}
