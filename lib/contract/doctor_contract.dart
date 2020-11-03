6df50ed3cc9d1fb7a7f76a131b159b23a9bbe099
quifar <risqufaris@gmail.com>
2020-11-03 10:00:45 +0700

    create doctor_contract.dart

---
+++ lib/contract/doctor_contract.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DoctorContractView{
+  onSuccessDoctorData(List<DocumentSnapshot> value){}
+  onErrorDoctorData(error){}
+}
+
+class DoctorContractPresenter{
+  getDoctorData(){}
+  loadDoctorData(){}
+}
