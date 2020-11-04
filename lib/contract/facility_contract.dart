6c214cebe6b12b7ec0a3c055ffc7fccdcd9cb0e5
quifar <risqufaris@gmail.com>
2020-11-04 13:26:37 +0700

    create facility_contract.dart

---
+++ lib/contract/facility_contract.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FacilityContractView{
+  onSuccessFacilityData(List<DocumentSnapshot> value){}
+  onErrorFacilityData(error){}
+}
+
+class FacilityContractPresenter{
+  getFacilityData(){}
+  loadFacilityData(){}
+}
