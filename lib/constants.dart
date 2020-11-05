7ddf6cf853192654a2fb6b669367a6f2605cca19
quifar <risqufaris@gmail.com>
2020-11-05 20:45:49 +0700

    update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,8 +2,11 @@ import 'package:flutter/cupertino.dart';
 
 class Constants{
   static const Color whiteColor = Color(0xffFFFFFF);
+  static const Color darkGreyColor = Color(0xff7F7F7F);
   static const Color greyColor = Color(0xffD0D0D0);
   static const Color greyColorTab = Color(0xffF2F2F2);
+  static const Color greyColorGuide = Color(0xff999999);
+  static const Color greyColorGuideIndicator = Color(0xffC4C4C4);
   static const Color blueColor = Color(0xff2962FF);
   static const Color blackColor = Color(0xff1E1E1D);
   static const Color redColor = Color(0xffFF5E20);
