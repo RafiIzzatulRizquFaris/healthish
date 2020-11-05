40e8e09531a689028d80babd2802c6e2cf220be1
quifar <risqufaris@gmail.com>
2020-11-05 17:31:14 +0700

    create guide_contract.dart

---
+++ lib/contract/guide_contract.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class GuideContractView{
+  onSuccessGuideData(List<DocumentSnapshot> value){}
+  onErrorGuideData(error){}
+}
+
+class GuideContractPresenter{
+  getGuideData(){}
+  loadGuideData(){}
+}
