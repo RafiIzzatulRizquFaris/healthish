0932615741a17fbf6a7ea3282b91c9fcbe7faebf
quifar <risqufaris@gmail.com>
2020-11-10 21:40:56 +0700

    create add_patient_contract.dart

---
+++ lib/contract/add_patient_contract.dart
@@ -0,0 +1,9 @@
+class AddPatientContractView{
+  onSuccessAddPatient(String status) {}
+  onErrorAddPatient(error) {}
+}
+
+class AddPatientContractPresenter{
+  getAddPatientData(String idUser, String name, String gender, String status){}
+  loadAddPatientData(String idUser, String name, String gender, String status){}
+}
